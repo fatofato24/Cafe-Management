0baee4237a30f3722e68c750533fe0fc0b8b9b1f
SanaAmin324 <sanaamin529@gmail.com>
2024-11-23 13:20:11 +0500

    server wala masla

--- database/connection.php
+++ database/connection.php
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
  <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-=======
-<?php
-$servername = "10.7.49.247";
-$username = "cimsSF";
-$password = "fatiboi";
->>>>>>> 8e6f8cc4f7ce5d2aab56bbafa7c5c47a84fbd537
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=inventory", $username, $password);
